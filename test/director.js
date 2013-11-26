02864bc6150122a477fbd51318079ae7f2005e7e
Sebi <sebastian.stanciu@sparktech.ro>
2013-11-26 11:19:05 +0200

    JSHintrc.

--- test/director.js
+++ test/director.js
@@ -2,11 +2,9 @@ var assert = require('assert'),
     Director = require('../lib/director');
 
 describe('Director', function () {
-  var mockery,
-    client;
-
-  before(function () {
-    var boshUrl = 'http://api.bosh.me/',
+    var mockery,
+        client,
+        boshUrl = 'http://api.bosh.me/',
         user = {id: '123'},
         userToken = 'dXNlcjpzZWNyZXQ=',
         boshPort = '25555',
@@ -14,68 +12,70 @@ describe('Director', function () {
         stageName = 'testing',
         boshPassword = 'secret';
 
-    mockery = require('mockery');
-    mockery.enable({ useCleanCache: true,
-      warnOnReplace: false,
-      warnOnUnregistered: false });
+    before(function () {
+        mockery = require('mockery');
+        mockery.enable({ useCleanCache: true,
+          warnOnReplace: false,
+          warnOnUnregistered: false });
 
-    client = new Director({ user: user,
-        userToken: userToken,
-        boshPort: boshPort,
-        boshUrl: boshUrl,
-        boshUser: boshUser,
-        boshPassword: boshPassword,
-        stageName: stageName
-    });
-  });
-
-  after(function () {
-    mockery.disable();
-  });
-
-  it('should query BOSH for status info', function(done) {
-    mockery.registerMock('request', function(obj, callback) {
-      assert.deepEqual(obj, {
-        rejectUnauthorized: false,
-        requestCert: true,
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: 'Basic ' + userToken},
-        url: boshUrl + 'info'
-      });
-
-       process.nextTick(function () {
-          callback(null, { statusCode: 200 });
-      });
+        client = new Director({ user: user,
+            userToken: userToken,
+            boshPort: boshPort,
+            boshUrl: boshUrl,
+            boshUser: boshUser,
+            boshPassword: boshPassword,
+            stageName: stageName
+        });
     });
 
-    client.getStatus(function(err, body) {
-      assert(!err, err);
-      done();
+    after(function () {
+        mockery.disable();
     });
-  });
 
-  it('should query BOSH for stemcell info', function(done) {
-    mockery.registerMock('request', function(obj, callback) {
-        assert.deepEqual(obj, {
-          requestUnauthorized: false,
-          requestCert: true,
-          headers: {
-            'Content-Type': 'applicaiton/json',
-            authorization: 'Basic ' + userToken
-          },
-          url: boshUrl + 'stemcells'
+    it('should query BOSH for status info', function (done) {
+        mockery.registerMock('request', function (obj, callback) {
+            assert.deepEqual(obj, {
+                rejectUnauthorized: false,
+                requestCert: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: 'Basic ' + userToken
+                },
+                url: boshUrl + 'info'
+            });
+
+            process.nextTick(function () {
+                callback(null, { statusCode: 200 });
+            });
         });
 
-        process.nextTick(function () {
-          callback(null, { statusCode: 200 });
-      });
+        client.getStatus(function (err, body) {
+            assert(!err, err);
+            done();
+        });
     });
 
-    client.stemcells(function(err, body) {
-      assert(!err, err);
-      done();
+    it('should query BOSH for stemcell info', function (done) {
+        mockery.registerMock('request', function (obj, callback) {
+            assert.deepEqual(obj, {
+                requestUnauthorized: false,
+                requestCert: true,
+                headers: {
+                    'Content-Type': 'applicaiton/json',
+                    authorization: 'Basic ' + userToken
+                },
+                url: boshUrl + 'stemcells'
+            });
+
+            process.nextTick(function () {
+                callback(null, { statusCode: 200 });
+            });
+        });
+
+        client.stemcells(function (err, body) {
+            assert(!err, err);
+            done();
+        });
     });
-  });
 
 });
